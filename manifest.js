bf60617ef0b488889c311c87c61968ea62d06163
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-18 18:08:50 +0800

    First Pieces of Code

---
+++ manifest.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    header: 'Resources',
+    items: [
+      {
+        name: 'Workshops',
+        icon: 'idea',
+        external: true,
+        url: 'https://workshops.hackclub.com'
+      },
+      {
+        name: 'Slack Community',
+        icon: 'slack',
+        external: true,
+        url: 'https://hackclub.com/slack'
+      },
+      {
+        name: 'Events',
+        icon: 'event-code',
+        external: true,
+        url: 'https://events.hackclub.com'
+      },
+      {
+        name: 'Hackathons',
+        icon: 'briefcase',
+        external: true,
+        url: 'https://hackathons.hackclub.com'
+      }
+    ]
+  },
+  {
+    header: 'Technical',
+    items: [
+      {
+        name: 'Theme',
+        icon: 'grid',
+        external: true,
+        url: 'https://theme.hackclub.com'
+      },
+      {
+        name: 'Theme Starter',
+        icon: 'help',
+        external: true,
+        url: 'https://github.com/hackclub/theme-starter'
+      },
+      {
+        name: 'DNS',
+        icon: 'web',
+        label: 'redeem a free hackclub.com subdomain',
+        external: true,
+        url: 'https://github.com/hackclub/dns'
+      }
+    ]
+  },
+  {
+    header: 'Promotions',
+    items: [
+      {
+        name: 'Replit',
+        icon: 'embed',
+        label: 'a free year of the Hacker Plan'
+      },
+      {
+        name: 'Postman',
+        icon: 'explore',
+        label: 'Free stuff'
+      },
+      {
+        name: 'Figma',
+        icon: 'photo',
+        label: 'Free stuff'
+      },
+      {
+        name: 'Stickers',
+        icon: 'sticker'
+      }
+    ]
+  },
+  {
+    header: 'Tools',
+    items: [
+      {
+        name: 'Zoom Pro Meetings',
+        icon: 'welcome'
+      },
+      {
+        name: 'Orpheus',
+        icon: 'sam',
+        label: 'Slack bot to support in running a club'
+      }
+    ]
+  }
+]
--- pages/_app.js
+++ pages/_app.js
@@ -1,111 +1,18 @@
-import "nextra-theme-docs/style.css";
-import "@hackclub/theme/fonts/reg-ital-bold.css";
-import "../public/style.css";
-import { signIn } from "next-auth/client";
-import { useRouter } from "next/router";
+import * as React from 'react'
+import NextApp from 'next/app'
+import '@hackclub/theme/fonts/reg-bold.css'
+import theme from '@hackclub/theme'
+import { ThemeProvider } from 'theme-ui'
+import ColorSwitcher from '../components/color-switcher'
 
-export default function Nextra({ Component, pageProps }) {
-  const router = useRouter();
-  return (
-    <>
-      <div
-        style={{
-          position: "fixed",
-          top: 0,
-          minHeight: "100vh",
-          width: "100vw",
-          alignItems: "center",
-          justifyContent: "center",
-          zIndex: "688",
-        }}
-        className="hidden-at-first"
-      >
-        <div
-          style={{
-            position: "fixed",
-            top: 0,
-            minHeight: "100vh",
-            width: "100vw",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: "888",
-            background:
-              "linear-gradient(300deg, rgba(1,28,54,0.5480567226890756) 0%, rgba(1,28,54,0.4248074229691877) 100%)",
-          }}
-          className="hidden-at-first"
-          onClick={() => router.reload()}
-        ></div>
-        <div
-          style={{
-            background: "white",
-            minWidth: "40vw",
-            maxWidth: "80vw",
-            width: "400px",
-            borderRadius: "0.25rem",
-            padding: "24px 16px",
-            textAlign: "center",
-            zIndex: "999",
-          }}
-        >
-          <h3
-            style={{
-              marginTop: "0rem",
-              lineHeight: "1.1",
-              marginBottom: "6px",
-            }}
-          >
-            This Code Requires A Hack Club Slack Account
-          </h3>
-          <div>
-            <code
-              onClick={() => signIn("slack")}
-              className="buttonAuth"
-              style={{
-                minWidth: "47%",
-                display: "inline-block",
-                marginTop: "6px",
-                marginRight: "2%",
-                fontFamily: `'Phantom Sans', 'Inter', system-ui, 'Helvetica Neue', Roboto,
-sans-serif !important`,
-                paddingLeft: "12px",
-                color: "black",
-                cursor: "pointer",
-              }}
-            >
-              Login with Slack
-            </code>
-            <a href="https://hackclub.com/slack">
-              <code
-                className="buttonAuth"
-                style={{
-                  minWidth: "47%",
-                  display: "inline-block",
-                  marginTop: "6px",
-                  fontFamily:
-                    "'Phantom Sans', 'Inter', system-ui, 'Helvetica Neue', Roboto, sans-serif !important",
-                  paddingLeft: "12px",
-                  color: "black",
-                }}
-              >
-                Create An Account
-              </code>
-            </a>{" "}
-          </div>
-        </div>
-      </div>
-      <style>
-        {`
-        
-        .buttonAuth:hover {
-          background: #ec3750;
-          color: white!important;
-        }
-        .hidden-at-first{
-          display: none
-        }
-        `}
-      </style>
-      <Component {...pageProps} />
-    </>
-  );
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <ColorSwitcher />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
 }
