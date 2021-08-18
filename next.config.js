117158256f8bcbd661eef132b959e2103f36aa63
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-19 01:21:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,12 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
-module.exports = withMDX({ pageExtensions: ['js', 'jsx', 'mdx'] })
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/home'
+      }
+    ]
+  }
+})
