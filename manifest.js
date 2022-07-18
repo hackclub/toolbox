128b44b0212660e352ca2eed2836f939bc3f4b8f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-07-18 14:45:32 -0400

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -47,7 +47,7 @@ export default [
       },
       {
         name: 'Bank',
-        icon: 'bank',
+        icon: 'bank-account',
         external: true,
         url: 'https://hackclub.com/bank',
         forUseBy: 'everyone'
