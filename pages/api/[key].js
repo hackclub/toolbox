6369d941f384eaac18dfaa9b5423df1c80f3f218
Echo <github@3kh0.net>
2026-05-11 10:46:22 -0400

    Patch to use normal airtable plug

--- pages/api/[key].js
+++ pages/api/[key].js
@@ -1,15 +1,13 @@
 import { getSession } from 'next-auth/react'
-const AirtablePlus = require('airtable-plus')
+const Airtable = require('airtable')
 
 export default async function handler(req, res) {
   const session = await getSession({ req })
   if (session != null) {
-    const airtable = new AirtablePlus({
-      baseID: 'appvp3mpqIrosrqdP',
-      apiKey: process.env.AIRTABLE,
-      tableName: 'Codes'
-    })
-    const record = await airtable.find(req.query.key)
+    const base = new Airtable({ apiKey: process.env.AIRTABLE }).base(
+      'appvp3mpqIrosrqdP'
+    )
+    const record = await base('Codes').find(req.query.key)
     res.json({ key: record.fields.key })
   } else res.json({ key: '👁' })
 }
