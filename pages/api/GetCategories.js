d1be4a0341828d58b8f1ea56d22b3289ecabee66
Mahmoud1205 <mahmoudwalid1205@outlook.com>
2026-05-14 22:02:13 +0300

    add APIs for getting toolbox items

---
+++ pages/api/GetCategories.js
@@ -0,0 +1,30 @@
+import manifest from '../../manifest.js';
+
+/**
+ * @brief Get categories list.
+ * Category format:
+ * {
+ *   name: "Category Name",
+ *   id: n
+ * }
+ */
+function getCategories() {
+	let categories = [];
+
+	for (let i = 0; i < manifest.length; i++) {
+		const entry = manifest[i];
+		categories.push({ name: entry.category, id: i });
+	}
+
+	return categories;
+}
+
+export default function handler(req, res) {
+	if (req.method !== "GET") {
+		res.setHeader("Allow", ["GET"]);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
+		return;
+	}
+
+	res.status(200).json(getCategories());
+}
